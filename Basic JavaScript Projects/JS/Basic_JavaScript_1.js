68d460801cf989af1cd403eef10c3ed381cd1a2d
lansford88 <rplansford@gmail.com>
2020-07-06 18:15:40 -0700

    Step 59 Push

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,24 @@
+var I = "Itachi Uchiha"
+var I = I.fontcolor("Red"); // Changes Itachi Uchiha Red//
+    window.alert(I);
+    document.write(I);
+
+var S = "Sasuke Uchiha"
+var S = S.fontcolor("DarkBlue");//Changes Sasuke's Name to Dark Blue color//
+    document.write(S);
+
+var blues = "I have the blues.";
+var blues = blues.fontcolor("green");
+    document.write(blues);
+
+    document.write(54+77);//This is an expression//
+
+function My_First_Function () {
+    var str = " This text is green!";
+    var result = str.fontcolor("green");
+    document.getElementById("Green_Text"). innerHTML = result;
+}
+
+var UchihaClan = EldestSon ="Itachi Uchiha", YoungestSon ="Saskuke Uchiha", BestFriend ="Shisui Uchiha";
+    document.write(BestFriend)
+    document.write(BestFriend + EldestSon)
